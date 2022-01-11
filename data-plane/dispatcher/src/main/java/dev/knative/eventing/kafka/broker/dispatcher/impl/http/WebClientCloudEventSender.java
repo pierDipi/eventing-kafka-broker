3c6e7ccc12dee64daa3097ebff8023eac55c5149
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-01-11 11:59:02 +0100

    Align dispatcher metrics to Eventing core filter metrics
    See https://knative.dev/docs/eventing/observability/metrics/eventing-metrics/#broker-filter
    
    Eventing Core uses `broker_name` and `trigger_name` for the tag
    but since we have Trigger/KafkaSource/Subscription, I'm using
    `name` and `consumer_name` instead.
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/http/WebClientCloudEventSender.java
+++ data-plane/dispatcher/src/main/java/dev/knative/eventing/kafka/broker/dispatcher/impl/http/WebClientCloudEventSender.java
@@ -17,6 +17,7 @@ package dev.knative.eventing.kafka.broker.dispatcher.impl.http;
 
 import dev.knative.eventing.kafka.broker.core.tracing.TracingSpan;
 import dev.knative.eventing.kafka.broker.dispatcher.CloudEventSender;
+import dev.knative.eventing.kafka.broker.dispatcher.impl.ResponseFailureException;
 import io.cloudevents.CloudEvent;
 import io.cloudevents.http.vertx.VertxMessageFactory;
 import io.cloudevents.rw.CloudEventRWException;
@@ -77,6 +78,7 @@ public final class WebClientCloudEventSender implements CloudEventSender {
     });
   }
 
+  @SuppressWarnings("rawtypes")
   private void send(final CloudEvent event, final Promise<HttpResponse<Buffer>> breaker) {
     VertxMessageFactory
       .createWriter(client.postAbs(target))
@@ -89,7 +91,10 @@ public final class WebClientCloudEventSender implements CloudEventSender {
 
         if (isRetryableStatusCode(response.statusCode())) {
           logError(event, response);
-          breaker.tryFail("response status code is not 2xx - got: " + response.statusCode());
+          breaker.tryFail(new ResponseFailureException(
+            (java.net.http.HttpResponse) response,
+            "Received failure response, status code: " + response.statusCode())
+          );
           return;
         }
 

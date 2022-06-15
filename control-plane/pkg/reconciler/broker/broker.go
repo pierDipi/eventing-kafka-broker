7b58495ceb7dfcc52732113d03a53dfe0c7aba3d
Ali Ok <aliok@redhat.com>
2022-06-15 16:37:58 +0300

    Fallback mechanism for the old external topic annotation for broker (#301)

--- control-plane/pkg/reconciler/broker/broker.go
+++ control-plane/pkg/reconciler/broker/broker.go
@@ -49,6 +49,9 @@ const (
 	// TopicPrefix is the Kafka Broker topic prefix - (topic name: knative-broker-<broker-namespace>-<broker-name>).
 	TopicPrefix = "knative-broker-"
 
+	// NOTE: this may go away in a future release
+	LegacyExternalTopicAnnotation = "x-kafka.eventing.knative.dev/external.topic"
+
 	// ExternalTopicAnnotation for using external kafka topic for the broker
 	ExternalTopicAnnotation = "kafka.eventing.knative.dev/external.topic"
 )
@@ -528,5 +531,9 @@ func finalizerCM(object metav1.Object) string {
 
 func isExternalTopic(broker *eventing.Broker) (string, bool) {
 	topicAnnotationValue, ok := broker.Annotations[ExternalTopicAnnotation]
+	if !ok {
+		// fallback to the old annotation
+		topicAnnotationValue, ok = broker.Annotations[LegacyExternalTopicAnnotation]
+	}
 	return topicAnnotationValue, ok
 }

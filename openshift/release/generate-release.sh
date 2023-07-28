be951f8b5da5beee7ae0a79b8d243296279ad51f
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-07-28 12:27:06 +0200

    Add eventshub service port name
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- openshift/release/generate-release.sh
+++ openshift/release/generate-release.sh
@@ -7,6 +7,7 @@ source $(dirname $0)/resolve.sh
 GITHUB_ACTIONS=true $(dirname $0)/../../hack/update-codegen.sh
 git apply openshift/patches/disable-ko-publish-rekt.patch
 git apply openshift/patches/override-min-version.patch
+git apply openshift/patches/add-eventshub-port-name.patch
 
 # Eventing core will bring the config tracing ConfigMap, so remove it from heret
 rm -f control-plane/config/eventing-kafka-broker/200-controller/100-config-tracing.yaml

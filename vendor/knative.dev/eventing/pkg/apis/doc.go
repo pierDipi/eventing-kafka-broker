982479716c58f3b7d4be805be0f81c388a35c2e8
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-09-04 17:45:31 +0200

    [release-v1.9] Point to Eventing midstream (#808)
    * Point to Eventing midstream
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Autoscaler fix
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Pod passed to evictor might be nil
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    ---------
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- vendor/knative.dev/eventing/pkg/apis/doc.go
+++ vendor/knative.dev/eventing/pkg/apis/doc.go
@@ -1,11 +1,11 @@
 /*
-Copyright 2020 The Knative Authors
+Copyright 2021 The Knative Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,16 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package flags
+// This file is need to work around a gengo/go1.20 issue
+// details https://github.com/ahmetb/gen-crd-api-reference-docs/issues/61
 
-// EventingEnvironmentFlags holds the e2e flags needed only by the eventing repo.
-type EventingEnvironmentFlags struct {
-	Channels
-	Sources
-	Brokers
-	PipeFile        string
-	ReadyFile       string
-	BrokerName      string
-	BrokerNamespace string
-	ReuseNamespace  bool
-}
+package apis

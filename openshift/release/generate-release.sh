74d949870ca4357b93368ff2b70c3fc072fcebbe
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-09-07 10:58:35 +0200

    Clean up reserved from resources that have been scheduled
    In a recent testing run, we've noticed we have have a scheduled
    `ConsumerGroup` [1] (see placements) being considered having
    reserved replicas in a different pod [2].
    
    That makes the scheduler think that there is no space but the
    autoscaler says we have enough space to hold every virtual
    replica.
    
    [1]
    ```
    $ k describe consumergroups -n ks-multi-ksvc-0 c9ee3490-5b4b-4d11-87af-8cb2219d9fe3
    Name:         c9ee3490-5b4b-4d11-87af-8cb2219d9fe3
    Namespace:    ks-multi-ksvc-0
    ...
    Status:
      Conditions:
        Last Transition Time:  2023-09-06T19:58:27Z
        Reason:                Autoscaler is disabled
        Status:                True
        Type:                  Autoscaler
        Last Transition Time:  2023-09-06T21:41:13Z
        Status:                True
        Type:                  Consumers
        Last Transition Time:  2023-09-06T19:58:27Z
        Status:                True
        Type:                  ConsumersScheduled
        Last Transition Time:  2023-09-06T21:41:13Z
        Status:                True
        Type:                  Ready
      Observed Generation:     1
      Placements:
        Pod Name:      kafka-source-dispatcher-6
        Vreplicas:     4
        Pod Name:      kafka-source-dispatcher-7
        Vreplicas:     4
      Replicas:        8
      Subscriber Uri:  http://receiver5-2.ks-multi-ksvc-0.svc.cluster.local
    Events:            <none>
    ```
    
    [2]
    ```
        "ks-multi-ksvc-0/c9ee3490-5b4b-4d11-87af-8cb2219d9fe3": {
          "kafka-source-dispatcher-3": 8
        },
    ```
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- openshift/release/generate-release.sh
+++ openshift/release/generate-release.sh
@@ -10,6 +10,7 @@ git apply openshift/patches/override-min-version.patch
 git apply openshift/patches/autoscaler_fix.patch
 git apply openshift/patches/remove_resource_version_check.patch
 git apply openshift/patches/autoscaler_leader_log.patch
+git apply openshift/patches/cleanup_reserved_from_deleted_and_non_pending_vpods.patch
 
 # Eventing core will bring the config tracing ConfigMap, so remove it from heret
 rm -f control-plane/config/eventing-kafka-broker/200-controller/100-config-tracing.yaml
